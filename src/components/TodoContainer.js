80039c4cb4ff9f00ba7e8a02e3a3469ea934abc3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-27 18:37:41 +0300

    Implement editing functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -53,6 +53,17 @@ class TodoContainer extends React.Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo;
+      })
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -63,6 +74,7 @@ class TodoContainer extends React.Component {
             todos={this.state.todos}
             handleChangeProps={this.handleChange}
             deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
           />
         </div>
       </div>
