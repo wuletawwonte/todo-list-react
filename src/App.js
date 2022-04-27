ee3cc628981d227566d2ea461621466e9b1a2ec2
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-27 12:39:34 +0300

    Add all component files inside the component folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Hello React!</h2>
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello React!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
