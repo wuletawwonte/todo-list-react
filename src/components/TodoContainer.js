0f6f2adc5febebfc3f1fa3f10224f735869b9d3c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-28 10:40:13 +0300

    Add About and NotMatch pages in the pages folder

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,7 @@ import TodoList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
+import { Route, Switch } from "react-router-dom";
 
 class TodoContainer extends React.Component {
   state = {
@@ -39,52 +40,48 @@ class TodoContainer extends React.Component {
 
   setUpdate = (updatedTitle, id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
-        if(todo.id === id) {
-          todo.title = updatedTitle
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
         }
         return todo;
-      })
-    })
-  }
-
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-  //   .then(response => response.json())
-  //   .then(data => this.setState({ todos: data }));
-  // }
+      }),
+    });
+  };
 
   componentDidMount() {
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
     if (loadedTodos) {
       this.setState({
-        todos: loadedTodos
-      })
+        todos: loadedTodos,
+      });
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.todos !== this.state.todos) {
-      const temp = JSON.stringify(this.state.todos)
-      localStorage.setItem("todos", temp)
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
     }
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={this.addTodoItem} />
-          <TodoList
-            todos={this.state.todos}
-            handleChangeProps={this.handleChange}
-            deleteTodoProps={this.delTodo}
-            setUpdate={this.setUpdate}
-          />
+      <Route exact path="/">
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo addTodoProps={this.addTodoItem} />
+            <TodoList
+              todos={this.state.todos}
+              handleChangeProps={this.handleChange}
+              deleteTodoProps={this.delTodo}
+              setUpdate={this.setUpdate}
+            />
+          </div>
         </div>
-      </div>
+      </Route>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import TodoContainer from './components/TodoContainer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>
 );
