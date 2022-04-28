266df19c5c7813587d7becda884e84f21a6dc76f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-28 10:26:02 +0300

    Add componentDidUpdate to store todos data to localstorage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -54,6 +54,13 @@ class TodoContainer extends React.Component {
     .then(data => this.setState({ todos: data }));
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+
   render() {
     return (
       <div className="container">
