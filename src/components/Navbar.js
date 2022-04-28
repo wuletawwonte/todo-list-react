e7369224642181244e23fcb3c65ff5d42477c3f6
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-28 11:38:18 +0300

    Add sidebar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import links from "../routes";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
 
   return (
     <nav className="navBar">
-      <ul>
+      <button onClick={handleToggle}>{navbarOpen ? (
+        <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+      ) : (
+        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+      )}
+      </button>      
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         {links.map((link) => {
           return (
             <li key={link.id}>
-              <NavLink to={link.path}  className={({ isActive }) => (isActive ? 'active-link' : null)} >{link.text}</NavLink>
+              <NavLink to={link.path} onClick={() => closeMenu()} className={({ isActive }) => (isActive ? 'active-link' : null)} >{link.text}</NavLink>
             </li>
           );
         })}
