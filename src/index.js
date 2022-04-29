abc5fbc14c7b8d07a04f68da5f2d6137e059b041
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-29 15:15:37 +0300

    Implement suggestion from reviewer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './functionBased/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
-import About from './functionBased/pages/About';
-import NotMatch from './functionBased/pages/NotMatch';
-import Navbar from './functionBased/components/Navbar';
-import SinglePage from './functionBased/pages/SinglePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<TodoContainer />} />
-        <Route path="/about/" element={<About />}>
-          <Route path=":slug" element={<SinglePage />} />
-        </Route>
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
+      <TodoContainer />
     </Router>
   </React.StrictMode>,
 );
